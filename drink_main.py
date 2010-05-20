7e755cce8f78499ee242f5132f4004204a3e8597
danielz <admin@dz-macbook.local>
2010-05-20 23:28:22 +0300

    Removed Google Account auth test code

--- drink_main.py
+++ drink_main.py
@@ -2,7 +2,6 @@ import logging, os
 
 # Google App Engine imports.
 from google.appengine.ext.webapp import util
-
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -11,16 +10,10 @@ from google.appengine.api import users
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+        # This generates the sample page 
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         template_args={}
         self.response.out.write(template.render(path, template_args))
-        user = users.get_current_user()
-
-#		if user:
-#			self.response.headers['Content-Type'] = 'text/plain'
-#			self.response.out.write('Hello, ' + user.user_id())
-#		else:
-#			self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp.WSGIApplication(
                                     [('/', MainPage)],
