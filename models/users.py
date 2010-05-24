1d9e8da2c8c9d48df7b769002255f5690b060be7
danielz <admin@dz-macbook.local>
2010-05-24 08:45:52 +0300

    TODO in models/user.py. Let's add username.

--- models/users.py
+++ models/users.py
@@ -2,6 +2,7 @@
 from google.appengine.ext import db
 
 class User(db.Model):
+    # TODO: Let's add username and authenticate on it instead of email
     email = db.EmailProperty()
     password = db.StringProperty()
     
