d44e01573e987c6be5698834946ce9150c71372f
danielz <admin@dz-macbook.local>
2010-05-26 22:29:34 +0300

    Moved login back to separate page.

--- controllers/home.py
+++ controllers/home.py
@@ -14,19 +14,3 @@ class MainPage(webapp.RequestHandler):
             model["user_name"] = user.username
         self.response.out.write(template.render('views/index.html', model))
 
-    def post(self):
-        username = self.request.get("username")
-        password = self.request.get("password")
-
-        # load user by username and password...
-        user = User.all()\
-                .filter("username =", username)\
-                .filter("password =", password)\
-                .get()
-
-        if user:
-            set_current_user(user)
-            self.redirect("/")
-        else:
-            # TODO: return to filled login form with error message
-            self.response.out.write("FAIL")
