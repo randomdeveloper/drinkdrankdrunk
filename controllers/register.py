045918befbabc38690d1b8cf3599266a2a22f4e1
Random Developer <randomdeveloper@yandex.ru>
2010-05-27 02:13:53 +0300

    Basic validation on register form

--- controllers/register.py
+++ controllers/register.py
@@ -9,25 +9,59 @@ from models.users import User
 
 class Register(webapp.RequestHandler):
     def get(self):
-        self.response.out.write(template.render("views/register.html", {}))
-        
+        model = RegisterModel()
+        self.view(model)
+
+    # todo: pass form data as method parameters        
     def post(self):
         username = self.request.get("username")
         password = self.request.get("password")
+        confirm_password = self.request.get("confirm_password")
         email = self.request.get("email")
 
-        # todo: add validation
+        # validate data
+        model = RegisterModel()
+        model.user_name_empty = (username == "")
+        # todo: email validation (user@domain etc..)
+        model.email_invalid = (email == "")
+        model.password_empty = (password == "")
+        model.passwords_dont_match = (password != confirm_password)
+
+        # if errors, redisplay form with error markers
+        if model.not_valid():
+            model.user_name = username
+            model.email = email
+            self.view(model)
+            return
+
+        # save new user        
         user = User()
         user.username = username
         # todo: hash password
         user.password = password
         user.email = email
 
-        # save new user
         user.put()
 
         # put him into session
         set_current_user(user)
 
         # redirect to the home page
-        self.redirect("/")        
+        self.redirect("/")
+
+    def view(self, model):
+        self.response.out.write(template.render("views/register.html", {"model": model}))
+
+        
+class RegisterModel:
+    user_name = ""
+    email = ""
+
+    user_name_empty = False
+    email_invalid = False
+    password_empty = False
+    passwords_dont_match = False
+
+    def not_valid(self):
+        return self.user_name_empty or self.email_invalid\
+               or self.password_empty or self.passwords_dont_match
